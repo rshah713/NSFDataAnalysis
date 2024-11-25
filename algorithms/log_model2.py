b56486be3491824be8992693ed5f725133e83ba9
AfshanZubia05 <aa3526@njit.edu>
2024-11-25 00:33:30 -0500

    added ethnicity variable

--- algorithms/log_model2.py
+++ algorithms/log_model2.py
@@ -10,11 +10,11 @@ df = pd.read_excel("condensed_data.xlsx")
 df = df.drop_duplicates(keep='first')
 df.columns = df.columns.str.strip()
 df = df.rename(columns={'(DV) Student \nGender': '(DV) Student Gender'})
-print(df[["NATIONALS", "(DV) Student Gender", "DV (Race)"]])
+print(df[["NATIONALS", "(DV) Student Gender", "DV (Race)"], "DV (Ethnicity)"])
 print(df['NATIONALS'].value_counts())
 #print(df.columns)
 
-feature_cols = ['(DV) Student Gender', 'DV (Race)']
+feature_cols = ['(DV) Student Gender', 'DV (Race)', 'DV (Ethnicity)']
 X = df[feature_cols]
 y = df.NATIONALS
 
@@ -27,11 +27,11 @@ logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
 
 print("\nModel Coefficients (weights):")
-print("Gender (1=Female), Race(1=Hispanic)")
+print("Gender (1=Female), Race(1=African American), Ethnicity(1=Hispanic)")
 print(logreg.coef_)
 
 print("\nIntercept:")
-print("Likelihood of getting to nationals if both dummy values are 0 i.e. male nonHispanic")
+print("Likelihood of getting to nationals if all dummy values are 0 i.e. male nonHispanic nonBlack")
 print(logreg.intercept_)
 print("Converting log_odds value to a probability using logistic function...")
 print("0.0343") # why is it hardcoded?
@@ -61,6 +61,7 @@ coefficients = logreg.coef_[0]
 t_scores = coefficients / standard_errors
 print(f"{feature_cols[0]}: Coefficient = {coefficients[0]:.4f}, SE = {standard_errors[0]:.4f}, T-Stat = {t_scores[0]:.4f}")
 print(f"{feature_cols[1]}: Coefficient = {coefficients[1]:.4f}, SE = {standard_errors[1]:.4f}, T-Stat = {t_scores[1]:.4f}")
+print(f"{feature_cols[2]}: Coefficient = {coefficients[1]:.4f}, SE = {standard_errors[1]:.4f}, T-Stat = {t_scores[1]:.4f}")
 
 # print("\nClassification Report:")
 # print(classification_report(y_test, y_pred))
